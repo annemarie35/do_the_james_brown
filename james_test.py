af711f2f94c4bd7b70b1be8c1a5df21344ab03a8
annemarie35 <annemariee@free.fr>
2015-12-08 15:47:18 +0100

    deleted test unused

--- james_test.py
+++ james_test.py
@@ -10,32 +10,5 @@ class TestJames(unittest.TestCase):
     def test_james_answer_to_question(self):
         self.assertEqual(self.james.ask("How are you?"), "Sure")
 
-    #def test_bob_answer_to_yelling(self):
-    #    self.assertEqual(self.bob.ask("HELLO"), "Woah")
-
-    # def test_bob_answer_to_yelling_question(self):
-    #     self.assertEqual(self.bob.ask("HELLO?"), "Woah")
-
-    # def test_bob_answer_to_yelling_question_with_trailing_spaces(self):
-    #     self.assertEqual(self.bob.ask("HELLO?   "), "Woah")
-
-    # def test_bob_answer_to_asking_question_with_trailing_spaces(self):
-    #     self.assertEqual(self.bob.ask("hello?   "), "Sure")
-
-    # def test_bob_answer_to_stating_nothing(self):
-    #     self.assertEqual(self.bob.ask(" "), "Fine")
-
-    # def test_bob_answer_to_stating_nothing_very_long(self):
-    #     self.assertEqual(self.bob.ask("            "), "Fine")
-
-    # def test_bob_answer_to_no_tone_sentence(self):
-    #     self.assertEqual(self.bob.ask("Go buy me some Club Maté."), "Whatever")
-
-    # def test_bob_answer_to_stating_numbers(self):
-    #     self.assertEqual(self.bob.ask("1, 2, 3"), "Whatever")
-
-    # def test_bob_answer_to_question_with_anarchic_punctuation_and_capitalization(self):
-    #     self.assertEqual(self.bob.ask("hEllO, hOw Are: yOu?   "), "Sure")
-
 if __name__ == '__main__':
     unittest.main()
